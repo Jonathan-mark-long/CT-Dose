d741747cd0a820e746c6def3cc8d6650b1e809d7
Jonathan Long <j.long@thelondonclinic.co.uk>
2022-12-16 14:34:34 +0000

    Adding sagital and coronal dose planes

--- CT_Planes.cs
+++ CT_Planes.cs
@@ -94,13 +94,21 @@ namespace Testing_CT_Images
 
             List<int> Transverse_Dose = new List<int>(); // List of dose points to generate bitmap from.
 
+            List<int> Sagital_Dose = new List<int>();
+            List<int> Coronal_Dose = new List<int>();
+
+
 
             int Transverse_slicenumber = (int)((153.0/CT.ZRes) - beam.IsocenterPosition.z);
 
             int Transverse_Dose_slice = 50; // Slice number to get planar dose.
+            int Sagital_Dose_slice = 50; // Slice number to get planar dose.
+            int Coronal_Dose_slice = 50; // Slice number to get planar dose.
 
-            int Coronal_slicenumber =  (int)(((262.0 + beam.IsocenterPosition.x) / CT.XRes) );
-            int Sagital_slicenumebr = (int)(((200.0 + beam.IsocenterPosition.y) / CT.YRes) );
+
+
+            int Sagital_slicenumber =  (int)(((262.0 + beam.IsocenterPosition.x) / CT.XRes) );
+            int Coronal_slicenumebr = (int)(((200.0 + beam.IsocenterPosition.y) / CT.YRes) );
             int[,] pixels = new int[CT.XSize, CT.YSize];
             int[,] doses = new int[dose.XSize, dose.YSize]; // integer array to store dose voxels.
 
@@ -119,11 +127,11 @@ namespace Testing_CT_Images
                         {
                             Tranverse_List.Add(value);
                         }
-                        if (i == Coronal_slicenumber)
+                        if (i == Sagital_slicenumber)
                         {
                             Sagital_List.Add(value);
                         }
-                        if (j == Sagital_slicenumebr)
+                        if (j == Coronal_slicenumebr)
                         {
                             Coronal_List.Add(value);
                         }
@@ -151,6 +159,14 @@ namespace Testing_CT_Images
                         {
                             Transverse_Dose.Add(dose_value); // add value to the list.
                         }
+                        if (i == Sagital_Dose_slice)
+                        {
+                            Sagital_Dose.Add(dose_value);
+                        }
+                        if (j == Coronal_Dose_slice)
+                        {
+                            Coronal_Dose.Add(dose_value);
+                        }
 
                     }
 
@@ -169,9 +185,15 @@ namespace Testing_CT_Images
             var sagital = BuildSagitalImage( CT, Sagital_List);
             SourcetoPng(sagital, "sagital");
 
+            var Sagital_DoseMap = BuildSagitalDoseImage(dose, Sagital_Dose); // Generate bitmap.
+            SourcetoPng(Sagital_DoseMap, "sagitalDose"); // Generate png.
+
 
             var coronal = BuildCoronalImage( CT, Coronal_List);
             SourcetoPng(coronal, "coronal");
+
+            var Coronal_DoseMap = BuildCoronalDoseImage(dose, Coronal_Dose); // Generate bitmap.
+            SourcetoPng(Coronal_DoseMap, "coronalDose"); // Generate png.
 
 
         }
@@ -246,6 +268,30 @@ namespace Testing_CT_Images
             return writeableBitmap;
 
         }
+
+        private static WriteableBitmap BuildSagitalDoseImage(Dose dose, List<int> List)
+        {
+
+            var max = List.Max();
+            var min = List.Min();
+
+            System.Windows.Media.PixelFormat format = PixelFormats.Gray8;
+            int stride = (dose.YSize * format.BitsPerPixel + 7) / 8;
+            byte[] image_bytes = new byte[stride * dose.ZSize];
+
+            for (int i = 0; i < List.Count; i++)
+            {
+                double value = List.ElementAt(i);
+                image_bytes[i] = Convert.ToByte(255 * ((value - min) / (max - min)));
+            }
+
+            BitmapSource source = BitmapSource.Create(dose.YSize, dose.ZSize, 25.4 / dose.XRes, 25.4 / dose.ZRes, format, null, image_bytes, stride);
+
+            WriteableBitmap writeableBitmap = new WriteableBitmap(source);
+
+            return writeableBitmap;
+
+        }
         private static WriteableBitmap BuildCoronalImage( VMS.TPS.Common.Model.API.Image CT, List<int> List)
         {
 
@@ -263,6 +309,30 @@ namespace Testing_CT_Images
             }
 
             BitmapSource source = BitmapSource.Create(CT.XSize, CT.ZSize, 25.4 / CT.YRes, 25.4 / CT.ZRes, format, null, image_bytes, stride);
+
+            WriteableBitmap writeableBitmap = new WriteableBitmap(source);
+
+            return writeableBitmap;
+
+        }
+
+        private static WriteableBitmap BuildCoronalDoseImage(Dose dose, List<int> List)
+        {
+
+            var max = List.Max();
+            var min = List.Min();
+
+            System.Windows.Media.PixelFormat format = PixelFormats.Gray8;
+            int stride = (dose.XSize * format.BitsPerPixel + 7) / 8;
+            byte[] image_bytes = new byte[stride * dose.ZSize];
+
+            for (int i = 0; i < List.Count; i++)
+            {
+                double value = List.ElementAt(i);
+                image_bytes[i] = Convert.ToByte(255 * ((value - min) / (max - min)));
+            }
+
+            BitmapSource source = BitmapSource.Create(dose.XSize, dose.ZSize, 25.4 / dose.YRes, 25.4 / dose.ZRes, format, null, image_bytes, stride);
 
             WriteableBitmap writeableBitmap = new WriteableBitmap(source);
 
